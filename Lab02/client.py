9b9fc7b344f6ef3a5ecfa87b2a2abd3da22136c7
joshua02 <josha.wilkinson@gmail.com>
2026-02-16 09:53:39 -0600

    Added python client

---
+++ Lab02/client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+HOST = "192.168.1.70"
+PORT = 8080
+
+
+def rx_loop(sock):
+    while True:
+        data = sock.recv(128)
+        print("Received: " + data.decode("ascii"))
+
+def tx_loop(sock):
+    while True:
+        # no input validation
+        input_text = input("")
+        sock.send(input_text.encode("ascii"))
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    print(f"Connected to {HOST}:{PORT}")
+
+    # spawn a new thread to handle recieving
+    recv_thread = threading.Thread(target=rx_loop, args=(client_socket,))
+    recv_thread.start()
+
+    # handle transmitting on main thread
+    tx_loop(client_socket)
+
+if __name__ == "__main__":
+    main()
